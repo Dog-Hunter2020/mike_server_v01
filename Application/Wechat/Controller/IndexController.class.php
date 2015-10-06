5520c53f1d3a925c515ea28fef142d7f6bc1d58b
1016990109 <1016990109@qq.com>
2015-10-06 14:18:01 +0800

    test

--- Application/Wechat/Controller/IndexController.class.php
+++ Application/Wechat/Controller/IndexController.class.php
@@ -3,8 +3,23 @@ namespace Wechat\Controller;
 use Think\Controller;
 //控制微信页面的跳转
 class IndexController extends Controller {
+	private $wechatWebController=new wechatWebController();
+	public function __init(){
+
+	}
+
+
     public function index(){
     	$this->assign('test','hehe');
     	$this->display('/teacher_create_test/?s=/openID/123');
+    }
+
+    public function testForTeacher(){
+    	//judge is the test is on
+    		$openid=I('openid');
+    		$courselist=$this->wechatWebController->getCourseList($openid);
+    		$this->assign('courselist',$courselist);
+    		$this->display('/teacher_create_test');	
+    	//
     }
 }
