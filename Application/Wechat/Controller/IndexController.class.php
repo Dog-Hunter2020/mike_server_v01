31cef157e38d1ded965c2683b13ff5e4d4216830
1016990109 <1016990109@qq.com>
2015-10-06 16:50:05 +0800

    change indexController

--- Application/Wechat/Controller/IndexController.class.php
+++ Application/Wechat/Controller/IndexController.class.php
@@ -3,9 +3,15 @@ namespace Wechat\Controller;
 use Think\Controller;
 //控制微信页面的跳转
 class IndexController extends Controller {
-	private $wechatWebController=new wechatWebController();
-	public function __init(){
+	private $wechatWebController;
+    private $TEST_STATUS_ON=1;
+    private $TEST_STATUS_CLOSED=-1;
+    private $TEST_STATUS_NOTEXIST=0;
+    private $teacherCreateTestUrl,$teacherTestOnUrl,$teacherTestResultUrl;
+    private $keyTestResult='test_result',
 
+	public function __initialize(){
+        $this->wechatWebController=new WechatWebController();
 	}
 
 
@@ -16,10 +22,38 @@ class IndexController extends Controller {
 
     public function testForTeacher(){
     	//judge is the test is on
-    		$openid=I('openid');
+    	    $openid=I('openid');
+            $identify=I('identify');
+            $this->wechatWebController=new WechatWebController();
+    		$testStatus=$this->wechatWebController->isTestOn($openid,$identify);
+            switch($testStatus['status']){
+                case $this->TEST_STATUS_CLOSED:
+                    $this->assign('test_result');
+            }
+
     		$courselist=$this->wechatWebController->getCourseList($openid);
     		$this->assign('courselist',$courselist);
     		$this->display('/teacher_create_test');	
     	//
+    }
+
+    public function testForStudent(){
+
+    }
+
+    public function countForTeacher(){
+
+    }
+
+    public function countForStudent(){
+
+    }
+
+    public function announceForTeacher(){
+
+    }
+
+    public function announceForStudent(){
+
     }
 }
